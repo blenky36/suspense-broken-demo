417ec42b6bbdadac0a067373b4bcf789c446b0f8
Phil Blenkinsop <philblenk@hotmail.co.uk>
2024-01-05 21:38:19 +0000

    update readme

--- app/page.tsx
+++ app/page.tsx
@@ -31,10 +31,10 @@ export default function Home() {
         <h1>PARALLEL ROUTES</h1>
         <p className="mt-12">This button is a client side navigation using next/link. It is blocked on navigation until the first promise is resolved (5s delay).</p>
         <Link href='/parallel' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mt-4 px-4 rounded">Link Nav</Link>
-        <p className="mt-12">This button is a client side navigation using push from next/navigation useRouter. It is blocked on navigation until the first promise is resolved (5s delay).</p>
-        <button onClick={(e) => onSubmitParallel(e, '/parallel')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mt-4 px-4 rounded">Client push nav</button>
-        <p className="mt-12">This button is a client side navigation using push from next/navigation useRouter. It doesn&apos;t get blocked on navigation, but the loading boundary only covers one of the dynamic routes.</p>
-        <button onClick={(e) => onSubmitParallel(e, '/parallel-with-loading')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mt-4 px-4 rounded">Client push nav</button>
+        <p className="mt-12">This button is a client side navigation using push from next/navigation useRouter. It is blocked on navigation until the first promise is resolved (5s delay) as there is no loading.tsx in /parallel.</p>
+        <button onClick={(e) => onSubmitParallel(e, '/parallel')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mt-4 px-4 rounded">Client push no loading.tsx</button>
+        <p className="mt-12">This button is a client side navigation using push from next/navigation useRouter. It doesn&apos;t get blocked on navigation, as there is a loading.tsx in /parallel-with-loading but the loading boundary only covers one of the dynamic routes.</p>
+        <button onClick={(e) => onSubmitParallel(e, '/parallel-with-loading')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mt-4 px-4 rounded">Client push with loading.tsx</button>
       </section>
     </main>
   )
