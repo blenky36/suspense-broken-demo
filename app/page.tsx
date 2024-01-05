57f46d99b1ecd40a9b6f8c376177373cc870d145
Phil Blenkinsop <philblenk@hotmail.co.uk>
2024-01-05 21:42:04 +0000

    Add spacing

--- app/page.tsx
+++ app/page.tsx
@@ -21,18 +21,25 @@ export default function Home() {
       <section className='flex flex-col items-center justify-center w-full'>
         <h1>SUSPENSE</h1>
         <p className="mb-4">This button will submit a form on the server and then navigate to /suspense using next/navigation. Navigation gets blocked for 5s whilst the server actions on /suspense resolve. No fallback UI from the Suspense is shown.</p>
+
         <ExampleForm />
         <p className="mt-12">This button is a client side navigation using next/link. It behaves as expected showing fallback UI whilst the promises resolve.</p>
+        
         <Link href='/suspense' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mt-4 px-4 rounded">Link Nav</Link>
+        
         <p className="mt-12">This button is a client side navigation using push from next/navigation useRouter. It behaves as expected showing fallback UI whilst the promises resolve.</p>
         <button onClick={onSubmitSuspense} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mt-4 px-4 rounded">Client push nav</button>
       </section>
+
       <section className='flex flex-col items-center justify-center w-full'>
         <h1>PARALLEL ROUTES</h1>
         <p className="mt-12">This button is a client side navigation using next/link. It is blocked on navigation until the first promise is resolved (5s delay).</p>
+
         <Link href='/parallel' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mt-4 px-4 rounded">Link Nav</Link>
         <p className="mt-12">This button is a client side navigation using push from next/navigation useRouter. It is blocked on navigation until the first promise is resolved (5s delay) as there is no loading.tsx in /parallel.</p>
+
         <button onClick={(e) => onSubmitParallel(e, '/parallel')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mt-4 px-4 rounded">Client push no loading.tsx</button>
+
         <p className="mt-12">This button is a client side navigation using push from next/navigation useRouter. It doesn&apos;t get blocked on navigation, as there is a loading.tsx in /parallel-with-loading but the loading boundary only covers one of the dynamic routes.</p>
         <button onClick={(e) => onSubmitParallel(e, '/parallel-with-loading')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mt-4 px-4 rounded">Client push with loading.tsx</button>
       </section>
